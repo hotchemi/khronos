4f13efd2991694a67938b78002e43584a298b0a5
hotchemi <hot.chemistry21@gmail.com>
2016-03-24 13:51:16 +0900

    Modify to use singleton calendar.

--- src/main/kotlin/khronos/Duration.kt
+++ src/main/kotlin/khronos/Duration.kt
@@ -9,7 +9,6 @@ class Duration(internal val unit: Int, internal val value: Int) {
     val since = calculate(from = Date(), value = value)
 
     private fun calculate(from: Date, value: Int): Date {
-        val calendar = Calendar.getInstance()
         calendar.time = from
         calendar.add(unit, value)
         return calendar.time
