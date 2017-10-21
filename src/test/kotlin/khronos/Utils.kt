5d1ed87984446fa3794db7fd975215f1184ae669
Thomas DE BONA <thomas.debona@gmail.com>
2017-10-21 13:29:42 +0200

    Make assertEquals as precise as Date (precise to milliseconds)

--- src/test/kotlin/khronos/Utils.kt
+++ src/test/kotlin/khronos/Utils.kt
@@ -1,10 +1,15 @@
 package khronos
 
 import org.junit.Assert
+import java.text.SimpleDateFormat
 import java.util.*
 
+internal val iso8601: SimpleDateFormat by lazy {
+    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+}
+
 fun assertEquals(expected: Date, actual: Date) {
-    Assert.assertEquals(expected.toString(), actual.toString())
+    Assert.assertEquals(iso8601.format(expected), iso8601.format(actual))
 }
 
 fun assertEquals(expected: Duration, actual: Duration) {
