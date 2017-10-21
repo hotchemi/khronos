60b17d30cdad48f76950d499b851ba9609c4f598
Thomas DE BONA <thomas.debona@gmail.com>
2017-10-21 13:36:42 +0200

    Make Date extensions and Dates precise to milliseconds

--- src/test/kotlin/khronos/StringExtensionsTest.kt
+++ src/test/kotlin/khronos/StringExtensionsTest.kt
@@ -9,7 +9,7 @@ class StringExtensionsTest {
 
     @Test fun toDate() {
         assertEquals(
-                expected = Dates.of(year = 1987, month = 6, day = 2, hour = 0, minute = 0, second = 0),
+                expected = Dates.of(year = 1987, month = 6, day = 2, hour = 0, minute = 0, second = 0, millisecond = 0),
                 actual = "1987-06-02".toDate("yyyy-MM-dd"))
     }
 
